28104f909ca9a7f5b61307b8eb0c27fea16fd449
Allison Yeh <yehas1@uci.edu>
2024-08-11 19:01:28 -0700

    Added the other pages and updated the font on the main page

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // function clicked(){
 //     alert("You have mail")
 // }
+
+function loadPage(page) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', page, true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            document.getElementById('content').innerHTML = xhr.responseText;
+        } else {
+            document.getElementById('content').innerHTML = 'Page not found.';
+        }
+    };
+    xhr.send();
+}
